33c4b906036bc8a8febd63725b55851660c8aaeb
Aliyah <78444693+Demonstrablee@users.noreply.github.com>
2024-11-06 23:31:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CalculatorAndroid
+ Following Android Studio Course a Tip Calculator for calculating how much to tip on a bill
---
+++ app/src/main/java/com/example/calculatorandroid/MainActivity.kt
@@ -0,0 +1,178 @@
+package com.example.calculatorandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.calculatorandroid.ui.theme.CalculatorAndroidTheme
+import java.text.NumberFormat
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CalculatorAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TipTimeLayout(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TipTimeLayout(modifier: Modifier = Modifier) {
+
+    Column (// layout composable
+        modifier = Modifier
+            .padding(horizontal =  40.dp)
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ){
+        var amountInput by remember {mutableStateOf(" ")} //hoisted state
+        var tipInput by remember { mutableStateOf("") }
+        var roundUp by remember { mutableStateOf(false) }
+
+        val amount = amountInput.toDoubleOrNull() ?: 0.0
+        val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+        val tip = calculateTip(amount, tipPercent, roundUp)
+
+
+        Text(text = "Calculate Tip")
+        EditNumberField(
+            value = amountInput,
+            label= R.string.bill_amount,
+            onValueChange = {amountInput= it},
+            modifier = Modifier
+                .padding(bottom = 32.dp).fillMaxWidth()
+        )
+        EditNumberField(
+            value = tipInput,
+            label = R.string.how_was_the_service,
+            onValueChange = {tipInput = it},
+            modifier = Modifier
+                .padding(bottom = 32.dp).fillMaxWidth()
+        )
+        RoundTheTipRow(
+            roundUp = roundUp,
+            onRoundUpChanged = {roundUp = it},
+            modifier = Modifier
+                .padding(bottom = 31.dp)
+        )
+        TipAmount(tip)
+    }
+}
+
+@Composable
+fun TipAmount(amnt: String, modifer : Modifier = Modifier) {
+    Text(
+        text = "Tip Amount: $amnt",
+        modifier = Modifier,
+        style = MaterialTheme.typography.displaySmall
+
+
+    )
+}
+
+@Composable
+fun Title(modifier: Modifier = Modifier) {
+    Text(
+        text = "Tip Calculator",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> (Unit),
+    modifier: Modifier,
+    @StringRes label: Int
+){
+
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        label = { Text(stringResource(label)) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+
+    )
+}
+@Composable
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.round_up_tip))
+        Switch(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged
+        )
+    }
+}
+
+private fun calculateTip(amount: Double, tipPercent: Double, roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+
+   //val user: String = if (readLine().toString() == null) readLine().toString() else "Andy"
+
+    CalculatorAndroidTheme {
+        TipTimeLayout()
+    }
+}
